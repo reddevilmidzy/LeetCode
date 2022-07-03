4397c68a9ab4c66413aae9015ff5b616ebea4ef8
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-07-03 14:00:19 +0900

    Time: 255 ms (28.67%), Space: 16.8 MB (20.40%) - LeetHub

---
+++ 136-single-number/136-single-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        num_dict = dict()
+        for i in nums:
+            if i not in num_dict:
+                num_dict[i] = 1
+            else:
+                num_dict[i] = 0
+                
+        for i in nums:
+            if num_dict[i] == 1:
+                return i
+        
