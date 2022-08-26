9270c8d1496c03a17dd0491ba326767a507493cb
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-08-26 23:05:35 +0900

    Time: 52 ms (66.31%), Space: 13.9 MB (26.74%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.py
+++ 869-reordered-power-of-2/869-reordered-power-of-2.py
@@ -2,9 +2,13 @@ class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
         two = 1
         n = str(n)
-        dict_two = defaultdict(list)
+        dict_two = dict()
         while len(str(two)) <= len(n):
-            dict_two[len(str(two))].append(two)
+            if len(str(two)) in dict_two:
+                dict_two[len(str(two))].append(two)
+            else:
+                dict_two[len(str(two))] = [two]
+                
             two *= 2
         
         for num in dict_two[len(n)]:
