16211694edea7d570a7bacd0239907348c3bbd39
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-08-30 21:30:13 +0900

    Time: 236 ms (50.90%), Space: 15 MB (90.58%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -3,6 +3,7 @@ class Solution:
         n = len(nums)
         if n == 1: return 0 # 무조건 갈 수 있음
         elif n == 2: return 1
+        elif n <= nums[0]: return 1
         l, r = 0, nums[0]
         times = 1
         while r < n - 1:
