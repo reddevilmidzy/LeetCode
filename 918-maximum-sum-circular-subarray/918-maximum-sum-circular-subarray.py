713a045cd3b87caa9c31b2162585a7a0a7dfd21e
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-08-31 21:40:02 +0900

    Time: 754 ms (61.68%), Space: 19 MB (77.14%) - LeetHub

--- 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.py
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.py
@@ -1,13 +1,14 @@
 class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
-        tot, min_sum, min_cur, max_sum, max_cur = sum(nums),nums[0],0,nums[0],0
+        tot, min_sum, min_cur, max_sum, max_cur = 0,nums[0],0,nums[0],0
         # 최대값을 구하는 방법은 배열에서 최대값을 찾거나, sum(배열)-최소값을 한 값이다.
         for i in nums:
             min_cur = min(min_cur+i, i)
             min_sum = min(min_sum, min_cur)
             max_cur = max(max_cur+i, i)
             max_sum = max(max_sum, max_cur)
-        
+            # sum 함수보다는 좀 더 빠른듯
+            tot += i
         # 원소중에 음수도 있기에 한번 체크해줌    
         return max(max_sum, tot-min_sum) if max_sum > 0 else max_sum
             
