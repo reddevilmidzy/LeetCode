a3cafa1df5416b0bb03010f3eeca52a29e778208
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-08-31 20:54:48 +0900

    Time: 1360 ms (22.98%), Space: 27.8 MB (78.18%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -2,8 +2,8 @@ class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         n = len(nums)
         # dp = [0]*n # dp table
-        now = nums[0] # now nums
-        ans = nums[0] # for ans nums
+        now =ans= nums[0] # now nums
+        # ans = nums[0] # for ans nums
         
         for i in range(1, n): # 0번째 인덱스 제외
             now = max(nums[i], now+nums[i]) # 현재부터 시작하는 것과, 이전의 값과 현재를 더하는것
