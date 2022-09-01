8699b7c4b1c4d034e6aabca0c32b4e9ed97c46d8
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-01 22:38:54 +0900

    Time: 216 ms (5.15%), Space: 14.3 MB (79.22%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -3,18 +3,12 @@ class Solution:
         max_cur = 1
         max_ans = nums[0]
         min_cur = 1
-        # min_ans = nums[0]
-        
-        # dp = [0,0]*(len(nums))
-        # dp[0] = [nums[0], nums[0]]
         
         for i in range(len(nums)):
             tmp = max_cur
             max_cur = max(nums[i], max_cur*nums[i], min_cur*nums[i])            
             min_cur = min(nums[i], min_cur*nums[i], tmp*nums[i])
             
-            max_ans = max(max_ans, max_cur)
-            # min_ans = min(min_ans, min_cur)
-            
+            max_ans = max(max_ans, max_cur)            
         
         return max_ans
