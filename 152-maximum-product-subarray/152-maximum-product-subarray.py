abebf11495fb591981caaf09cd6b11cd8940f4bf
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-01 22:38:18 +0900

    Time: 150 ms (33.83%), Space: 14.3 MB (79.22%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.py
+++ 152-maximum-product-subarray/152-maximum-product-subarray.py
@@ -3,7 +3,7 @@ class Solution:
         max_cur = 1
         max_ans = nums[0]
         min_cur = 1
-        min_ans = nums[0]
+        # min_ans = nums[0]
         
         # dp = [0,0]*(len(nums))
         # dp[0] = [nums[0], nums[0]]
@@ -14,7 +14,7 @@ class Solution:
             min_cur = min(nums[i], min_cur*nums[i], tmp*nums[i])
             
             max_ans = max(max_ans, max_cur)
-            min_ans = min(min_ans, min_cur)
+            # min_ans = min(min_ans, min_cur)
             
         
         return max_ans
