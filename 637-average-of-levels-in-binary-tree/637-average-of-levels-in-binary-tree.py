169ee669dcd095cde132b4961a207dc167d41ed6
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-02 20:21:02 +0900

    Time: 48 ms (97.38%), Space: 16.5 MB (46.07%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.py
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.py
@@ -6,13 +6,13 @@
 #         self.right = right
 class Solution:
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
-        q, ans = deque([root]), [] # 넣고 뺄 자료구조
-        while len(q):
+        q, ans = [root], [] # 넣고 뺄 자료구조
+        while len(q): # 빌때까지
             qlen, row = len(q), 0
             for i in range(qlen):
-                curr = q.popleft()
+                curr = q.pop(0)
                 row += curr.val
                 if curr.left: q.append(curr.left)
                 if curr.right: q.append(curr.right)
-            ans.append(row/qlen)
+            ans.append(row/qlen) # 평균 넣기
         return ans
