26ca09c14328765cf10e53c97cd9208b66213aee
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-02 22:45:51 +0900

    Time: 1937 ms (23.12%), Space: 25.1 MB (38.19%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,10 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        max_profit, min_price = 0,prices[0]
-        
-        for price in prices:
-            min_price = min(min_price, price)
-            profit = price - min_price
-            max_profit = max(max_profit, profit)
-        
+        left = 0 #사는 포인트
+        right = 1 # 파는 포인트
+        max_profit = 0
+        while right < len(prices):
+            currentProfit = prices[right] - prices[left] # 현재
+            if prices[left] < prices[right]:
+                max_profit =max(currentProfit,max_profit)
+            else: # 마이너스 난다면 다시 시작
+                left = right
+            right += 1
         return max_profit
