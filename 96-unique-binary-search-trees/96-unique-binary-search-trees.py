2b5c81a177ee35687d3806a13b546af19d5534fc
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-06 21:21:02 +0900

    Time: 37 ms (79.52%), Space: 13.9 MB (61.31%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.py
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.py
@@ -1,4 +1,4 @@
 class Solution:
     def numTrees(self, n: int) -> int:
-        return factorial(2*n)//factorial(n)//factorial(n)//(n+1)
+        return factorial(2*n)//(factorial(n)*factorial(n)*(n+1))
         # [(1,1), (2, 2), (3,5), (4, 14), (5, 42), (6, 132), (7,429)]
