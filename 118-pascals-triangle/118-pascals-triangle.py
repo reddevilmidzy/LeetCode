0806e6dd15bd4f5a5b48c3bdd4d1a854520fef3f
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-07 22:10:07 +0900

    Time: 54 ms (32.28%), Space: 13.8 MB (65.59%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -1,8 +1,8 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        n = numRows
-        dp = [[1] for _ in range(n)]
-        for i in range(1, n):
+        #n = numRows
+        dp = [[1] for _ in range(numRows)]
+        for i in range(1, numRows):
             for j in range(i-1):
                 dp[i].extend([dp[i-1][j]+dp[i-1][j+1]])
             dp[i].append(1)
