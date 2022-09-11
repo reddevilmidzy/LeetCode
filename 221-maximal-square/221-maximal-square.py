d71ea50d937b61ee31dd234e0a802b5a8fa029ef
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-11 17:19:37 +0900

    Time: 1124 ms (28.26%), Space: 17 MB (27.45%) - LeetHub

--- 221-maximal-square/221-maximal-square.py
+++ 221-maximal-square/221-maximal-square.py
@@ -3,6 +3,9 @@ class Solution:
         rows = len(matrix)
         cols = len(matrix[0])
         
+        if rows==1 and cols==1:
+            return matrix[0][0]
+        
         dp = [[0]*(cols+1) for _ in range(rows+1)]
         res = 0
         for r in range(rows):
