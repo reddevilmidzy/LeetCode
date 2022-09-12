c30ac371f86028ffc4d774cf7978a7392bd5ca3c
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-12 14:33:06 +0900

    Time: 102 ms (30.19%), Space: 14 MB (77.36%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.py
+++ 948-bag-of-tokens/948-bag-of-tokens.py
@@ -2,7 +2,7 @@ class Solution:
     def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
         res,cur = 0,0
         queue = deque(sorted(tokens))
-        # 정렬한다음에 가장 싼것을 구매
+        # 정렬한 다음에 가장 싼것을 구매
         while queue and (queue[0] <= power or cur):
             if queue[0] <= power:
                 power -= queue.popleft()
