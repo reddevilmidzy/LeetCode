77d65319da3c809111f0448ba46338587d7c331c
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-12 14:25:29 +0900

    Time: 127 ms (7.08%), Space: 13.9 MB (99.06%) - LeetHub

---
+++ 948-bag-of-tokens/948-bag-of-tokens.py
@@ -0,0 +1,17 @@
+class Solution:
+    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+        res = 0
+        cur = 0
+        d = collections.deque(sorted(tokens))
+        # 정렬한다음에 가장 싼것을 구매
+        while d and (d[0] <= power or cur):
+            if d[0] <= power:
+                power -= d.popleft()
+                cur += 1
+            else:
+                power += d.pop()
+                cur -= 1
+            
+            res = max(res, cur)
+        
+        return res
