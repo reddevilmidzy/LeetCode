36743edb90a44ae2c210d28314e02f92ce9c770b
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-14 23:51:42 +0900

    Time: 1004 ms (30.56%), Space: 13.8 MB (99.86%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,8 +1,9 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        te1_len = len(text1)
         m, n = len(text1), len(text2)
-        if m < n: return self.longestCommonSubsequence(text2, text1) # 길이가 2가 더 크다면
+        if m < n: 
+            n,m = m,n
+            text1,text2 = text2,text1
         dp = [0] * (n + 1)
         for i in range(1, m + 1):
             pre = 0
