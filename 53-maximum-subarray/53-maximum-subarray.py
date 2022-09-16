33cf3e6b81a129355c8570d125311d7f8d314f38
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-16 20:51:45 +0900

    Time: 1266 ms (39.59%), Space: 27.8 MB (77.70%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,11 +1,10 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        # n = len(nums)
-        now = ans= nums[0] # now nums
-        # ans = nums[0] # for ans nums
+        res = nums[0]
+        cur = nums[0]
         
-        for i in range(1, len(nums)): # 0번째 인덱스 제외
-            now = max(nums[i], now+nums[i]) # 현재부터 시작하는 것과, 이전의 값과 현재를 더하는것
-            ans = max(now, ans) # now 값이 갱신되어도 기억하고 있는 최대값
+        for i in range(1,len(nums)):
+            cur = max(nums[i], cur+nums[i])
+            res = max(cur, res)
             
-        return ans
+        return res
