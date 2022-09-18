c8d4dbc4892e224333ebe3b94b0fa3f85aa953b3
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-18 23:49:05 +0900

    Time: 441 ms (5.04%), Space: 16.2 MB (10.15%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -3,13 +3,13 @@ class Solution:
         n = len(height)
         # 정방향으로 갈때 가장 높은 거 담을 배열과, 역방향으로 갈 때 가장 높은 거 담을 배열
         maxLeft, maxRight = [0] * n, [0] * n
-        # 집어넣음
         for i in range(1, n):
             maxLeft[i] = max(height[i-1], maxLeft[i-1])
         for i in range(n-2, -1, -1):
             maxRight[i] = max(height[i+1], maxRight[i+1])
             
         ans = 0
+        
         for i in range(n):
             # 수심은 더 작은거 체크
             waterLevel = min(maxLeft[i], maxRight[i])
