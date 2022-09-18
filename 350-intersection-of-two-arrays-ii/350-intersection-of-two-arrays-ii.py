0df37e1366efcc6036d0b74c5e034b28b49d7bf2
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-18 23:59:09 +0900

    Time: 41 ms (99.57%), Space: 14.1 MB (50.37%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.py
@@ -2,7 +2,6 @@ class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
         cnt_nums1,cnt_nums2 = Counter(nums1),Counter(nums2)
         ans = []
-        for i in list(set(nums1)&set(nums2)):
-            ans.extend([i]*min(cnt_nums1[i], cnt_nums2[i]))
+        for i in list(set(nums1)&set(nums2)): ans.extend([i]*min(cnt_nums1[i], cnt_nums2[i]))
         return ans
         
