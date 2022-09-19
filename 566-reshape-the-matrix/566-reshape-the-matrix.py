bc8ee5ba80ded25d56877086a598b09386d81bce
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-19 20:56:36 +0900

    Time: 209 ms (15.60%), Space: 14.7 MB (81.37%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.py
+++ 566-reshape-the-matrix/566-reshape-the-matrix.py
@@ -3,21 +3,17 @@ class Solution:
         memo = []
         n = len(mat)
         m = len(mat[0])
-        for i in range(n):
-            for j in range(m):
-                #print(mat[i][j])
-                memo.append(mat[i][j])
-        
         if n*m!=r*c:
             return mat
         
-        #print(memo)
+        for i in range(n):
+            for j in range(m):
+                memo.append(mat[i][j])
+        
         ans = [[] for _ in range(r)]
         idx = 0
         for i in range(r):
-            #ans.append([])
             for j in range(c):
-                # print(memo[idx])
                 ans[i].append(memo[idx])
                 idx += 1
         return ans
