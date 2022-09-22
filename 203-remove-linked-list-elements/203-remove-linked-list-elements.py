89a9d0f08844202f5f0f32fde8a9fb23224bb54a
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2022-09-23 00:35:24 +0900

    Time: 223 ms (5.22%), Space: 17.8 MB (38.99%) - LeetHub

---
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        # 디스커스 참고...
+        # 리스트 노드 처리 방법을 아직 잘 모르겠다.
+        # 트리 노드와 비슷한 것 같다; .val 연산
+        dummy_head = ListNode(-1)
+        dummy_head.next = head
+        
+        current_node = dummy_head
+        while current_node.next != None:
+            if current_node.next.val == val:
+                current_node.next = current_node.next.next
+            else:
+                current_node = current_node.next
+                
+        return dummy_head.next
