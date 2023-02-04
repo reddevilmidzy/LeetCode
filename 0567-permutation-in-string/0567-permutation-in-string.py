2fa67ab0ec05ee9fe25954629d067afae2f4d283
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-02-04 19:05:35 +0900

    Time: 2720 ms (20.21%), Space: 13.9 MB (64.59%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -2,12 +2,11 @@ from collections import Counter
 
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        
         d1 = Counter(s1)
         k = len(s1)
-        for i in range(len(s2)):  # ---- O(n)
-            sub = s2[i:i+k]  # ------ O(k)
-            d2 = Counter(sub) # --- O(k)
+        for i in range(len(s2)):
+            sub = s2[i:i+k]
+            d2 = Counter(sub)
             if d1 == d2:
                 return True
         return False
