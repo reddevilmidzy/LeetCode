8bb02c2993b94dbabb87f58f2fb9119de0b2bff8
Cozy <78539407+reddevilmidzy@users.noreply.github.com>
2023-02-08 09:59:51 +0900

    Time: 130 ms (82.68%), Space: 15.2 MB (22.88%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,13 +1,17 @@
 class Solution:
-    def jump(self, nums: list[int]) -> int:
-        INF = int(1e9)
+    def jump(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [INF]*(n)
-        dp[0] = 0
-        for i in range(n):
-            for j in range(1,nums[i]+1):
-                if i+j < n:
-                    dp[i+j] = min(dp[i+j], dp[i]+1)
-                else:
-                    break
-        return dp[n-1]
+        if n <= 2: 
+            return n-1 # 무조건 갈 수 있음
+        if n <= nums[0]: 
+            return 1 # 1트에 성공하는 경우
+
+        l, r = 0, nums[0]
+        times = 1
+        
+        # 최근에 도착한 지점에서만 탐색
+        while r < n - 1:
+            times += 1
+            nxt = max(i + nums[i] for i in range(l, r + 1))
+            l, r = r, nxt
+        return times
