9a77b14efd88dd4e428c10a5809f89abdd4c7ec6
CHOI DONGGEUN <78539407+reddevilmidzy@users.noreply.github.com>
2024-02-01 16:15:09 +0900

    Time: 25 ms (38.13%), Space: 61.8 MB (5.21%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.java
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.java
@@ -6,9 +6,7 @@ class Solution {
         
         for (int i = 0; i < n - 2; i += 3) {
             if (nums[i+2] - nums[i] <= k) {
-                res[i/3][0] = nums[i];
-                res[i/3][1] = nums[i+1];
-                res[i/3][2] = nums[i+2];
+                res[i/3] = new int[]{nums[i], nums[i+1], nums[i+2]};
             } else {
                 return new int[0][0];
             }
