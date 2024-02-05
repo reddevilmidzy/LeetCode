14aaa1076c3d881c2a0f19a10239c9aefbfab6ad
CHOI DONGGEUN <78539407+reddevilmidzy@users.noreply.github.com>
2024-02-05 15:13:37 +0900

    Time: 24 ms (78.34%), Space: 11.7 MB (33.26%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        int cnt[26] = {};
+        for (int i = 0; i < s.length(); i++) {
+            cnt[s[i] - 'a']++;
+        }
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (cnt[s[i] - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
