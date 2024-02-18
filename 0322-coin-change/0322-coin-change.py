44c0eff9723645052541b27e9c1222280936a9f2
CHOI DONGGEUN <78539407+reddevilmidzy@users.noreply.github.com>
2024-02-18 13:27:00 +0900

    Time: 763 ms (75.37%), Space: 16.9 MB (79.55%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.py
@@ -0,0 +1,18 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        INF = int(1e9)
+        dp = [INF]*(amount+1)
+        arr = sorted([i for i in coins if i <= amount])
+        
+        if amount==0:
+            return 0
+        elif arr==[]:
+            return -1        
+        for coin in arr:
+            dp[coin] = 1
+        for i in range(arr[0]+1, amount+1):
+            for coin in arr:
+                if i > coin and dp[i-coin] != INF:
+                    dp[i] = min(dp[i-coin]+1,dp[i])
+        
+        return dp[amount] if dp[amount] != INF else -1
