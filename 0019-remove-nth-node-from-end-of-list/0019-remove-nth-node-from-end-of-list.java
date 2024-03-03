661c3eab96a3e2248a2a4e0da73c52039dfda9ad
Donggeun <78539407+reddevilmidzy@users.noreply.github.com>
2024-03-03 12:55:54 +0900

    Time: 0 ms (100%), Space: 41.3 MB (87.36%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.java
@@ -8,35 +8,6 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-/*
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-
-        fast = head
-        slow = head
-
-        # Move fast pointer n steps ahead
-        for _ in range(n):
-            
-            fast = fast.next
-            if not fast:
-                return head.next
-
-        # Move both fast and slow pointers until fast reaches the end
-        while fast.next:
-            fast = fast.next
-            slow = slow.next
-
-        # Remove the nth node from the end
-        slow.next = slow.next.next
-
-        return head
-*/
 
 class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
@@ -51,12 +22,12 @@ class Solution {
             }
         }
 
-        while (fast != null) {
+        while (fast != null && fast.next != null) {
             fast = fast.next;
             slow = slow.next;
         }
 
         slow.next = slow.next.next;
-        return slow;
+        return head;
     }
 }
