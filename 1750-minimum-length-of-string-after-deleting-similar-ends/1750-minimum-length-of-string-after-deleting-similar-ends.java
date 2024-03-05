4759a4e54d1559c5d8d48aa99a9413a91be433cf
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2024-03-05 20:58:06 +0900

    Time: 3 ms (100%), Space: 45 MB (73.02%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.java
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.java
@@ -3,30 +3,17 @@ class Solution {
         int n = s.length();
         int left = 0;
         int right = n-1;
-        if (n == 1) {
-            return 1;
-        }
-        while (left <= right) {
-            if (s.charAt(left) == s.charAt(right)) {
-                char leftChar = s.charAt(left);
-                while (left < n && leftChar == s.charAt(left)) {
-                    left++;
-                }
-                char rightChar = s.charAt(right);
-                while (0 <= right && rightChar == s.charAt(right)) {
-                    right--;
-                }
-                if (left == right) {
-                    return 1;
-                } else if (left > right) {
-                    return 0;
-                }
-            } else {
-                break;
+       
+        while (left < right && s.charAt(left) == s.charAt(right)) {
+            char c = s.charAt(left);
+
+            while (left <= right && s.charAt(left) == c) {
+                left++;
+            }
+            while (right > left && s.charAt(right) == c) {
+                right--;
             }
         }
-        System.out.println(left);
-        System.out.println(right);
         return right - left + 1;
     }
 }
