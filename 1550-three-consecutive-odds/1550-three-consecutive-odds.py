e3779daea2184f7040988301faf4e41e637246ef
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2024-07-01 17:27:48 +0900

    Time: 42 ms (85.64%), Space: 16.5 MB (93.64%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.py
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.py
@@ -1,8 +1,3 @@
 class Solution:
     def threeConsecutiveOdds(self, arr: List[int]) -> bool:
-        n = len(arr)
-        if n < 3: return False
-        for i in range(n-2):
-            if arr[i]%2 == arr[i+1]%2 == arr[i+2]%2 == 1:
-                return True
-        return False
+        return any(arr[i]%2 == arr[i+1]%2 == arr[i+2]%2 == 1 for i in range(len(arr)-2))
