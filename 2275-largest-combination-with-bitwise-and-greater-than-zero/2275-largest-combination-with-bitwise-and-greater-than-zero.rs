668bf4db4ee605a989ecce0d230db7378218a981
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2024-11-07 16:07:45 +0900

    Time: 9 ms (100%), Space: 3.5 MB (9.09%) - LeetHub

--- 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.rs
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.rs
@@ -1,19 +1,8 @@
 impl Solution {
     pub fn largest_combination(nums: Vec<i32>) -> i32 {
-        let n = nums.len();
-        let m = 24;
-
-        let mut res = 1;
-
-        for bit in 0..=m {
-            let mut tmp = 0;
-            for &num in &nums {
-                if (num & (1 << bit)) != 0 {
-                    tmp += 1;
-                }
-            }
-            res = res.max(tmp);
-        }
-        res
+        (0..24)
+            .map(|bit| nums.iter().filter(|&num| num & (1 << bit) != 0).count() as i32)
+            .max()
+            .unwrap_or(0)
     }
 }
