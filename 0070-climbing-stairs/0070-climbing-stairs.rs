f0f15caa73d42fc68a1e84844069f64cd69b0ace
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2024-11-17 00:25:13 +0900

    Time: 0 ms (100%), Space: 2.1 MB (26.03%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.rs
+++ 0070-climbing-stairs/0070-climbing-stairs.rs
@@ -1,12 +1,12 @@
 impl Solution {
     pub fn climb_stairs(n: i32) -> i32 {
-        let mut dp : Vec<i32> = vec![0; (n as usize).max(2) + 1];
+        let mut dp = vec![0; (n as usize).max(2) + 1];
         dp[1] = 1;
         dp[2] = 2;
 
         for i in 3..=n {
             dp[i as usize] = dp[i as usize -1] + dp[i as usize -2];
         }
-        dp[n as usize] as i32
+        dp[n as usize]
     }
 }
