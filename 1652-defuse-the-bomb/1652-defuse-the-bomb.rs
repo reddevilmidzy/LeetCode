1a2d95a09d56685e17cacdb71d52090000a073e5
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2024-11-18 13:01:16 +0900

    Time: 0 ms (100%), Space: 2.1 MB (61.11%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.rs
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.rs
@@ -2,22 +2,26 @@ impl Solution {
     pub fn decrypt(code: Vec<i32>, k: i32) -> Vec<i32> {
         let n : usize = code.len();
         let mut res = vec![0; n];
-        if k > 0 {
-            for i in 0..n {
-                let mut tmp = 0;
-                for j in (i+1)..=(i+k as usize) {
-                    tmp += code[j % n];
-                }
-                res[i] = tmp;
-            }
-        } else if k < 0 {
-            for i in 0..n {
-                let mut tmp = 0;
-                for j in 1..=(-k as usize) {
-                    tmp += code[(n + i - j) % n];
-                }
-                res[i] = tmp;
-            }
+        if k == 0 {
+            return res;
+        }
+        let mut st : usize = 1;
+        let mut ed : usize = k as usize;
+        let mut tot = 0;
+
+        if k < 0 {
+            st = n + k as usize;
+            ed = n - 1;
+        }
+        for i in st..=ed {
+            tot += code[i];
+        }
+        for i in 0..n {
+            res[i] = tot;
+            tot -= code[st % n];
+            tot += code[(ed + 1) % n];
+            st += 1;
+            ed += 1;
         }
         res
     }
