57d52b857ca5ff64c00163b2d6ed87e4889c5d38
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2024-12-26 16:08:55 +0900

    Time: 3 ms (75%), Space: 2.4 MB (41.67%) - LeetHub

--- 0494-target-sum/0494-target-sum.rs
+++ 0494-target-sum/0494-target-sum.rs
@@ -1,23 +1,26 @@
 impl Solution {
     pub fn find_target_sum_ways(nums: Vec<i32>, target: i32) -> i32 {
         let n : usize = nums.len();
-        let m : i32 = 1000;
-        let mut dp : Vec<Vec<i32>> = vec![vec![0; (m * 2 + 1) as usize]; n];
+        let m : i32 = nums.iter().sum();
+        let mut dp : Vec<i32> = vec![0; (m * 2 + 1) as usize];
 
-        dp[0][(nums[0] + m) as usize] = 1;
-        dp[0][(m - nums[0]) as usize] += 1;
+        dp[(nums[0] + m) as usize] = 1;
+        dp[(m - nums[0]) as usize] += 1;
 
         for i in 1..n {
-            for j in 0..=(m as usize * 2) {
-                if j as i32 + nums[i] <= 2 * m && dp[i-1][(j as i32 + nums[i]) as usize] != 0 {
-                    dp[i][j] += dp[i-1][(j as i32 + nums[i]) as usize];
+            let mut nxt_dp = vec![0; (m * 2 + 1) as usize];
+            for j in -m..=m {
+                if dp[(j + m) as usize] > 0 {
+                    nxt_dp[(j + nums[i] + m) as usize] += dp[(j + m) as usize];
+                    nxt_dp[(j - nums[i] + m) as usize] += dp[(j + m) as usize];
                 }
-                if 0 <= j as i32 - nums[i] && dp[i-1][(j as i32 - nums[i]) as usize] != 0 {
-                    dp[i][j] += dp[i-1][(j as i32 - nums[i]) as usize];
-                }
-
             }
+            dp = nxt_dp;
         }
-        dp[n-1][(target + m) as usize]
+        
+        if target.abs() > m {
+            return 0;
+        }
+        dp[(m + target) as usize]
     }
 }
