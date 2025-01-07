2e43a21b58c4a61954b08f3dd1dbe4b0513f4991
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2025-01-07 22:09:13 +0900

    Time: 0 ms (100%), Space: 2.4 MB (21.43%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.rs
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.rs
@@ -1,20 +1,16 @@
-use std::collections::HashSet;
-
 impl Solution {
     pub fn string_matching(words: Vec<String>) -> Vec<String> {
-        let mut res: HashSet<String> = HashSet::new();
+        let mut res: Vec<String> = Vec::new();
         let n: usize = words.len();
 
         for i in 0..n {
-            for j in i+1..n {
-                if words[i].contains(words[j].as_str()) {
-                    res.insert(words[j].clone());
-                } else if words[j].contains(words[i].as_str()) {
-                    res.insert(words[i].clone());
+            for j in 0..n {
+                if words[j].contains(words[i].as_str()) && i != j {
+                    res.push(words[i].clone());
+                    break;
                 }
             }
         }
-        let res: Vec<String> = res.iter().map(|x| x.clone()).collect();
         res
     }
 }
