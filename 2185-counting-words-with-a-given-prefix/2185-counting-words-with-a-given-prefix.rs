8ba10a8f95a6a201c2f4e13b9e9d35c6eb89ee23
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2025-01-09 16:07:47 +0900

    Time: 0 ms (100%), Space: 2.3 MB (83.33%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.rs
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.rs
@@ -1,11 +1,7 @@
 impl Solution {
     pub fn prefix_count(words: Vec<String>, pref: String) -> i32 {
-        let mut res = 0;
-        for word in words {
-            if word.starts_with(&pref) {
-                res += 1;
-            }
-        }
-        res
+        words.iter()
+            .filter(|x| x.starts_with(&pref))
+            .count() as i32
     }
 }
