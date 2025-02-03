78c6dce8aed20b56d5713399cdb41bce97b9a9cb
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2025-02-03 10:56:45 +0900

    Time: 0 ms (100%), Space: 2.4 MB (16.67%) - LeetHub

--- 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.rs
+++ 3105-longest-strictly-increasing-or-strictly-decreasing-subarray/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.rs
@@ -1,23 +1,25 @@
 impl Solution {
     pub fn longest_monotonic_subarray(nums: Vec<i32>) -> i32 {
-        let mut res = 0;
+        let mut res = 1;
         let n = nums.len();
 
-        let mut inc_dp = vec![1; n];
-        let mut dec_dp = vec![1; n];
+        let mut inc_dp = 1;
+        let mut dec_dp = 1;
 
         for i in 1..n {
             if nums[i-1] < nums[i] {
-                inc_dp[i] = inc_dp[i-1] + 1;
+                inc_dp += 1;
+            } else {
+                inc_dp = 1;
             }
             if nums[i-1] > nums[i] {
-                dec_dp[i] = dec_dp[i-1] + 1;
+                dec_dp += 1;
+            } else {
+                dec_dp = 1;
             }
+            res = res.max(inc_dp).max(dec_dp);
         }
 
-        for i in 0..n {
-            res = res.max(inc_dp[i]).max(dec_dp[i]);
-        }
         res
     }
 }
