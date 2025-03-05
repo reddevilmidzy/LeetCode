a23b8aeb334574679afb8d2e15bf52df79ab5975
Redddy <78539407+reddevilmidzy@users.noreply.github.com>
2025-03-05 20:39:09 +0900

    Time: 0 ms (100%), Space: 2.2 MB (33.33%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.rs
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.rs
@@ -1,11 +1,5 @@
 impl Solution {
     pub fn colored_cells(n: i32) -> i64 {
-        let mut res = 1;
-        let n = n as i64;
-        for i in 2..=n {
-            res += 4 * (i - 1);
-        }
-
-        res
+        1 + n as i64 * (n as i64 - 1) * 2
     }
 }
