d3ef4da9f019256b947531a816b85cb8d294167b
Redddy <midzy0228@gmail.com>
2026-09-06 15:52:26 +0900

    0115-DISTINCT-SUBSEQUENCES Stats: Time: 3 ms (54.84%), Space: 2.3 MB (41.94%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.rs
+++ 0115-distinct-subsequences/0115-distinct-subsequences.rs
@@ -5,22 +5,17 @@ impl Solution {
         let s = s.as_bytes();
         let t = t.as_bytes();
 
-        // dp[i][j] = s[i]ë²ì§¸ ìì ê¹ì§ ì¬ì©íê³ , t[j] ê¹ì§ ì§íëìì ë
-        let mut dp = vec![vec![0; m + 1]; n + 1];
-        for i in 0..n {
-            dp[i][0] = 1;
-        }
+        let mut dp = vec![0; m + 1];
+        dp[0] = 1;
 
         for i in 1..=n {
-            for j in 1..=m {
+            for j in (1..=m).rev() {
                 if s[i - 1] == t[j - 1] {
-                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-                } else {
-                    dp[i][j] = dp[i - 1][j];
+                    dp[j] += dp[j - 1];
                 }
             }
         }
 
-        dp[n][m]
+        dp[m]
     }
 }
